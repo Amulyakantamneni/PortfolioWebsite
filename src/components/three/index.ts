8e849d5ad2b0bfd197c26a5bc6ebb2c2cef68c47
Amulyakantamneni <72382578+Amulyakantamneni@users.noreply.github.com>
2026-01-24 22:12:14 -0500

    Create index.ts

---
+++ src/components/three/index.ts
@@ -0,0 +1,4 @@
+export { Scene3D } from "./Scene3D";
+export { PlaceholderGeometry } from "./PlaceholderGeometry";
+export { ClickZone3D } from "./ClickZone3D";
+export { Overlay3D } from "./Overlay3D";
